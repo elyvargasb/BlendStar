8aedf1ef89204d2700d36e485d1044105fc50a99
Eliana Vargas <elyvb_2010@hotmail.com>
2018-12-18 01:10:32 +0000

    header & footer
    same header and footer in all pages

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
-$( "a" ).addClass( "test" );
-$( "a" ).removeClass( "test" );
+$(function(){
+  $("#header-index").load("header-index.html");
+  $("#header").load("header.html");
+  $("#footer").load("footer.html");
+});
